af18411c3ad240e985b4044f18b284b5d2547cfd
Diogomadeira97 <diogomadeira@id.uff.br>
2025-01-26 17:53:56 -0300

    update

--- Conf/default/default-server.sh
+++ Conf/default/default-server.sh
@@ -14,7 +14,7 @@ echo -e "server{\n	listen 80 default_server;\n	listen [::]:80 default_server;\n\
 echo -e "server{\n	listen 80;\n	listen [::]:80;\n	server_name $1$2;\n	return 301 https://\$host\$request_uri;	\n}\n\nserver{\n	listen 443 ssl http2;\n	listen [::]:443 ssl http2;\n	server_name $1$2;\n	root /var/www/$1;\n\n	ssl_certificate /etc/letsencrypt/live/$1$2/fullchain.pem;\n	ssl_certificate_key /etc/letsencrypt/live/$1$2/privkey.pem;\n}" >> $1
 
 #Edit index.html of Domain.
-echo -e '\n       <title>'"$5"'</title>' >> index.html
+echo -e '\n        <title>'"$5"'</title>' >> index.html
 cat index_temp.html >> index.html
 
 #Create manifest.json to Domain.
