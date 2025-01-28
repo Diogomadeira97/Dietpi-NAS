76d7d929a9bc4eaeb1f304b51258c88da675c741
Diogomadeira97 <diogomadeira@id.uff.br>
2025-01-28 14:20:03 -0300

    update

--- Conf/default/default-user.sh
+++ Conf/default/default-user.sh
@@ -1,48 +1,44 @@
 #! /bin/bash
 
+passwd(){ < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo;}
+
 #Do it while have a User.
 for (( i=2; i<=$#; i++)); 
 do
 
     #User.
+    echo -e "#User $1.\n" >> PASSWD_$1.txt
     USER=${!i}
     echo -e "USER="${!i} >> PASSWD_$1.txt
-    USERPW=
+    USERPW=$(passwd)
     echo -e "$(echo "$USERPW")" >> PASSWD_$1.txt
-    USERSMBPW=
+    USERSMBPW=$(passwd)
     echo -e "$(echo "$USERSMBPW")" >> PASSWD_$1.txt
 
-    #User.
-    a=${!i}
-    #User Password.
-    b=${!x}
-    #User Samba Password.
-    c=${!y}
-
     #Add user.
-    sudo adduser --quiet --disabled-password --shell /bin/bash --home /home/$a --gecos "User" "$a"
-    echo "$a:"$(echo "$b")"" | sudo chpasswd
+    sudo adduser --quiet --disabled-password --shell /bin/bash --home /home/$USER --gecos "User" "$USER"
+    echo "$USER:"$(echo "$USERPW")"" | sudo chpasswd
 
     #Add Samba password.
-    (echo "$(echo "$c")"; echo "$(echo "$c")") | sudo smbpasswd -a -s $a
+    (echo "$(echo "$USERSMBPW")"; echo "$(echo "$USERSMBPW")") | sudo smbpasswd -a -s $USER
 
     #Put user in the default group.
-    sudo gpasswd -M $a $1_Cloud
+    sudo gpasswd -M $USER $1_Cloud
 
     #Go to Users folder and create default folders to the user.
     cd /mnt/Cloud/Users
-    sudo mkdir $a $a/Docs $a/Midias
+    sudo mkdir $USER $USER/Docs $USER/Midias
 
     #Set default permissions to user folder.
-    sudo chown -R $a:$1_Cloud $a
-    sudo chmod -R 775 $a
-    sudo setfacl -R -d -m u::rwx $a
-    sudo setfacl -R -d -m g::rwx $a
-    sudo setfacl -R -d -m o::r-x $a
+    sudo chown -R $USER:$1_Cloud $USER
+    sudo chmod -R 775 $USER
+    sudo setfacl -R -d -m u::rwx $USER
+    sudo setfacl -R -d -m g::rwx $USER
+    sudo setfacl -R -d -m o::r-x $USER
 
     #Set default permissions to user Docs folder.
-    cd $a
-    sudo chown -R $a:$a Docs
+    cd $USER
+    sudo chown -R $USER:$USER Docs
     sudo chmod -R 750 Docs
     sudo setfacl -R -d -m u::rwx Docs
     sudo setfacl -R -d -m g::r-x Docs
@@ -63,19 +59,19 @@ do
 
     #Add Samba share folders to the user.
     sudo cat /etc/samba/smb.conf >> smb.conf
-    sudo echo -e "\n\n#User $a\n\n[$a]\n	comment = $a\n	path = /mnt/Cloud/Users/$a\n	valid users = $a" >> smb.conf
+    sudo echo -e "\n\n#User $USER\n\n[$USER]\n	comment = $USER\n	path = /mnt/Cloud/Users/$USER\n	valid users = $USER" >> smb.conf
     sudo chown root:root smb.conf
     sudo chmod 644 smb.conf
     sudo mv smb.conf /etc/samba/smb.conf
 
     #Add user folders to immich.
     cd /mnt/Cloud/Data/Docker/immich-app
-    sudo echo -e "      - /mnt/Cloud/Users/$a/Midia/Midias-Anuais:/mnt/Cloud/Users/$a/Midia/Midias-Anuais:ro\n" >> docker-compose.yml
+    sudo echo -e "      - /mnt/Cloud/Users/$USER/Midia/Midias-Anuais:/mnt/Cloud/Users/$USER/Midia/Midias-Anuais:ro\n" >> docker-compose.yml
     sudo docker compose restart
 
     #Create a crontab to sync Immich with user folder.
-    echo -e "#! /bin/bash\n\nmv /mnt/Cloud/Data/Docker/immich-app/immich_files/library/$a/*  /mnt/Cloud/Users/$a/Midias/Midias-Anuais/immich\n\nchown -R $a:$a /mnt/Cloud/Users/$a/Midias/Midias-Anuais/immich" >> immich_cron_$a.sh
-    mv immich_cron_$a.sh /etc/cron.daily
-    chmod 750 /etc/cron.daily/immich_cron_$a.sh
+    echo -e "#! /bin/bash\n\nmv /mnt/Cloud/Data/Docker/immich-app/immich_files/library/$USER/*  /mnt/Cloud/Users/$USER/Midias/Midias-Anuais/immich\n\nchown -R $USER:$USER /mnt/Cloud/Users/$USER/Midias/Midias-Anuais/immich" >> immich_cron_$USER.sh
+    mv immich_cron_$USER.sh /etc/cron.daily
+    chmod 750 /etc/cron.daily/immich_cron_$USER.sh
 
 done
