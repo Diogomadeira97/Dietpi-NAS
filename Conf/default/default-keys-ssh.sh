e140091c0e4a75bf599f5c174742416ad4debf1e
Diogomadeira97 <diogomadeira@id.uff.br>
2025-01-31 00:39:58 -0300

    update

--- Conf/default/default-keys-ssh.sh
+++ Conf/default/default-keys-ssh.sh
@@ -7,7 +7,7 @@ sudo chmod 644 /etc/ssh/sshd_config.d/dietpi.conf
 sudo service sshd restart
 
 #Create folders to put the private keys.
-sudo mkdir /mnt/Cloud/Public/Keys_VPN /mnt/Cloud/Public/Keys_SSH
+sudo mkdir /mnt/Cloud/Public/Keys_SSH
 
 #Go to .ssh folder to create SSH Keys.
 cd ~/.ssh
@@ -15,7 +15,7 @@ cd ~/.ssh
 ARS=( "$@" )
 
 #Do it while have a Device.
-for (( i=2; i<$#; i++)); 
+for (( i=4; i<$#; i++));
 do
 
     #Device.
@@ -25,16 +25,13 @@ do
     sudo ssh-keygen -f $a -P ""
 
     #Copy the Device SSH key to admin user.
-    sudo ssh-copy-id -i $a.pub "$a@$1$2"
+    sudo ssh-copy-id -i $a.pub "$3@$1$2"
 
     #Change Device SSH key permissions.
     sudo chmod 777 $a
 
     #Move Device SSH Key to /mnt/Cloud/Keys_SSH and easily export with Dietpi-Dashboard or Samba.
     sudo mv $a /mnt/Cloud/Public/Keys_SSH
-
-    #Create a Device Wireguard Key.
-    sudo pivpn add -n $a
 
 done
 
@@ -46,8 +43,3 @@ sudo service sshd restart
 
 #Give right permissions to files and folder
 sudo chmod -R 777 /mnt/Cloud/Public/Keys_SSH
-
-#Move Device Wireguard Key to /mnt/Cloud/Keys_VPN and easily export with Dietpi-Dashboard or Samba.
-sudo chmod 777 /home/$3/configs/*
-sudo mv /home/$3/configs/* /mnt/Cloud/Public/Keys_VPN
-sudo rm -rf /home/$3/configs
