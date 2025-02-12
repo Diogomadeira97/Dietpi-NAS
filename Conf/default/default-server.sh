0fd3e7cd387fd3f83c26c52036df5276a0004a1e
Diogomadeira97 <diogomadeira@id.uff.br>
2025-02-12 10:43:55 -0300

    update

--- Conf/default/default-server.sh
+++ Conf/default/default-server.sh
@@ -143,7 +143,7 @@ item "gimp" "Editor de Imagens."
 #Passbolt.
 echo -e "server{\n	listen 80;\n	listen [::]:80;\n	server_name passbolt.$1$2;\n	return 301 https://\$host\$request_uri;	\n}\n\n" >> /etc/nginx/sites-available/passbolt
 echo -e "server{\n	listen 443 ssl http2;\n	listen [::]:443 ssl http2;\n	server_name passbolt.$1$2;\n	ssl_certificate /etc/letsencrypt/live/$1$2/fullchain.pem;\n	ssl_certificate_key /etc/letsencrypt/live/$1$2/privkey.pem;\n" >> /etc/nginx/sites-available/passbolt
-echo -e "        location / {\n                proxy_pass http://$1$2:3050/;\n        }\n}" >> /etc/nginx/sites-available/passbolt
+echo -e "        location / {\n                proxy_pass http://$1$2:8050/;\n        }\n}" >> /etc/nginx/sites-available/passbolt
 cd /etc/nginx/sites-enabled
 sudo ln -s /etc/nginx/sites-available/passbolt .
 echo -e "sudo iptables -A INPUT -p tcp ! -s $3 --dport 3050 -j DROP" >> /mnt/Cloud/Data/Commands/iptables_custom.sh
