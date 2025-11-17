6e90648ffc9f4fb476882a800caf618b767d5946
Diogomadeira97 <83920663+Diogomadeira97@users.noreply.github.com>
2025-11-17 18:58:10 -0300

    Update update_server.sh

--- Conf/default/update_server.sh
+++ Conf/default/update_server.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-#Update Fail2Ban, Dietpi-Dashboard, PiVPN(Wireguard), Unbound, AdGuard_Home, Samba_server, Transmission, Sonarr, Radarr, Prowlarr, Readarr, Bazarr, Jellyfin, Kavita, Nginx, LEMP, Docker, Docker_Compose, Portainer, Home-Assistant, Homer, Certbot, Samba Client and Nextcloud.
+#Update Fail2Ban, Dietpi-Dashboard, PiVPN(Wireguard), Unbound, AdGuard_Home, Samba_server, Transmission, Sonarr, Radarr, Prowlarr, Bazarr, Readarr, Jellyfin, Kavita, Nginx, LEMP, Docker, Docker_Compose, Portainer, Home-Assistant, Homer, Certbot, Samba Client and Nextcloud.
 sudo /boot/dietpi/dietpi-software reinstall 73 200 117 182 126 96 44 144 145 151 180 203 178 212 85 79 134 162 185 157 1 114 92
 
 #Update and upgrade pkgs
